601d250723c5fd09396d03b05c5ad500ffaeb5d4
jaivalos-dev <jaivalos.dev@gmail.com>
2024-03-05 12:42:44 -0600

    create-model

--- sign_and_show/models.py
+++ sign_and_show/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class document(models.Model):
+    docfile = models.FileField(upload_to='documents/%Y/%m/%d')
+    def __str__(self):
+        return self.docfile.name
